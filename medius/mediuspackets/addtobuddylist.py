1387e4cb10d4b9afb6be512cc4eb81c5c0b89153
jtjanecek <janecektyler@gmail.com>
2021-07-12 15:00:21 -0600

    Buddies and ignore list

--- medius/mediuspackets/addtobuddylist.py
+++ medius/mediuspackets/addtobuddylist.py
@@ -1,12 +1,16 @@
-
+from enums.enums import MediusEnum, CallbackStatus
 from utils import utils
+from medius.mediuspackets.addtobuddylistresponse import AddToBuddyListResponseSerializer
 
 class AddToBuddyListSerializer:
 	data_dict = [
-		{'name': 'mediusid', 'n_bytes': 2, 'cast': None}
+		{'name': 'mediusid', 'n_bytes': 2, 'cast': None},
+		{'name': 'message_id', 'n_bytes': MediusEnum.MESSAGEID_MAXLEN, 'cast': None}
 	]
 
 class AddToBuddyListHandler:
 	def process(self, serialized, monolith, con):
-		raise Exception('Unimplemented Handler: AddToBuddyListHandler')
-
+		return [AddToBuddyListResponseSerializer.build(
+				serialized['message_id'],
+				CallbackStatus.SUCCESS
+			)]
--- medius/mediuspackets/addtoignorelist.py
+++ medius/mediuspackets/addtoignorelist.py
@@ -1,12 +1,17 @@
-
+from enums.enums import MediusEnum, CallbackStatus
 from utils import utils
+from medius.mediuspackets.addtoignorelistresponse import AddToIgnoreListResponseSerializer
 
 class AddToIgnoreListSerializer:
 	data_dict = [
-		{'name': 'mediusid', 'n_bytes': 2, 'cast': None}
+		{'name': 'mediusid', 'n_bytes': 2, 'cast': None},
+		{'name': 'message_id', 'n_bytes': MediusEnum.MESSAGEID_MAXLEN, 'cast': None}
 	]
+
 
 class AddToIgnoreListHandler:
 	def process(self, serialized, monolith, con):
-		raise Exception('Unimplemented Handler: AddToIgnoreListHandler')
-
+		return [AddToIgnoreListResponseSerializer.build(
+				serialized['message_id'],
+				CallbackStatus.SUCCESS
+			)]
