57ab1b8df07434b451b3bb95c6ac6b5fed41570d
jtjanecek <janecektyler@gmail.com>
2021-07-11 21:26:35 -0600

    Add ignorelist

--- medius/mediuspackets/getignorelist.py
+++ medius/mediuspackets/getignorelist.py
@@ -1,12 +1,20 @@
-
+from enums.enums import MediusEnum, CallbackStatus, MediusPlayerStatus
 from utils import utils
+from medius.mediuspackets.getignorelistresponse import GetIgnoreListResponseSerializer
 
 class GetIgnoreListSerializer:
 	data_dict = [
-		{'name': 'mediusid', 'n_bytes': 2, 'cast': None}
+		{'name': 'mediusid', 'n_bytes': 2, 'cast': None},
+		{'name': 'message_id', 'n_bytes': MediusEnum.MESSAGEID_MAXLEN, 'cast': None}
 	]
 
 class GetIgnoreListHandler:
 	def process(self, serialized, monolith, con):
-		raise Exception('Unimplemented Handler: GetIgnoreListHandler')
-
+		return [GetIgnoreListResponseSerializer.build(
+			serialized['message_id'],
+			CallbackStatus.NO_RESULT,
+			0, # ignore account id
+			"", # account name
+			MediusPlayerStatus.MEDIUS_PLAYER_DISCONNECTED, 
+			1 # end of list
+		)]
--- medius/mediuspackets/getignorelistresponse.py
+++ medius/mediuspackets/getignorelistresponse.py
@@ -1,10 +1,33 @@
-
+from enums.enums import MediusIdEnum, MediusEnum
 from utils import utils
 
 class GetIgnoreListResponseSerializer:
 	data_dict = [
 		{'name': 'mediusid', 'n_bytes': 2, 'cast': None}
 	]
+
+	@classmethod
+	def build(self,			
+			message_id: bytes,
+			callback_status: int,
+			ignore_account_id: int,
+			username: str,
+			player_status: int,
+			end_of_list: int
+			):
+		packet = [
+			{'name': __name__},
+			{'mediusid': MediusIdEnum.GetIgnoreListResponse},
+			{'message_id': message_id},
+			{'buf': utils.bytes_from_hex("000000")},
+			{'callback_status': utils.int_to_bytes_little(4, callback_status)},
+			{'ignore_account_id': utils.int_to_bytes_little(4, ignore_account_id)},
+			{'username': utils.str_to_bytes(username, MediusEnum.ACCOUNTNAME_MAXLEN)},
+			{'player_status': utils.int_to_bytes_little(4, player_status)},
+			{'end_of_list': utils.int_to_bytes_little(4, end_of_list)}
+		]
+		return packet
+
 
 class GetIgnoreListResponseHandler:
 	def process(self, serialized, monolith, con):
