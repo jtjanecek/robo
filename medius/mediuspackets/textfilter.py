e1d861faca0b98ca6d5c416adfcac86d846e3e7d
jtjanecek <janecektyler@gmail.com>
2021-07-11 18:39:30 -0600

    Text filter packet

--- medius/mediuspackets/textfilter.py
+++ medius/mediuspackets/textfilter.py
@@ -1,12 +1,21 @@
-
+from enums.enums import MediusEnum, CallbackStatus
 from utils import utils
+from medius.mediuspackets.textfilterresponse import TextFilterResponseSerializer
 
 class TextFilterSerializer:
 	data_dict = [
-		{'name': 'mediusid', 'n_bytes': 2, 'cast': None}
+		{'name': 'mediusid', 'n_bytes': 2, 'cast': None},
+		{'name': 'message_id', 'n_bytes': MediusEnum.MESSAGEID_MAXLEN, 'cast': None},
+		{'name': 'session_key', 'n_bytes': MediusEnum.SESSIONKEY_MAXLEN, 'cast': None},
+		{'name': 'buf', 'n_bytes': 2, 'cast': None},
+		{'name': 'text_filter_type', 'n_bytes': 4, 'cast': utils.bytes_to_str},
+		{'name': 'text', 'n_bytes': MediusEnum.CHATMESSAGE_MAXLEN, 'cast': None}
 	]
 
 class TextFilterHandler:
 	def process(self, serialized, monolith, con):
-		raise Exception('Unimplemented Handler: TextFilterHandler')
-
+		return [TextFilterResponseSerializer.build(
+			serialized['message_id'],
+			serialized['text'],
+			CallbackStatus.PASS
+		)]
--- medius/mediuspackets/textfilterresponse.py
+++ medius/mediuspackets/textfilterresponse.py
@@ -1,10 +1,27 @@
-
+from enums.enums import MediusIdEnum
+from enums.enums import MediusEnum
 from utils import utils
 
 class TextFilterResponseSerializer:
 	data_dict = [
 		{'name': 'mediusid', 'n_bytes': 2, 'cast': None}
 	]
+
+	@classmethod
+	def build(self,
+			message_id,
+			text,
+			callback_status
+		):
+		packet = [
+			{'name': __name__},
+			{'mediusid': MediusIdEnum.TextFilterResponse},
+			{'message_id': message_id},
+			{'text': text},
+			{'buf': utils.bytes_from_hex("000000")},
+			{'callback_status': utils.int_to_bytes_little(4, callback_status)}
+		]
+		return packet
 
 class TextFilterResponseHandler:
 	def process(self, serialized, monolith, con):
