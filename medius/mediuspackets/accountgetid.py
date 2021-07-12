4b741ff0fb961ed4bd72d53b38626c19fdb31aa1
jtjanecek <janecektyler@gmail.com>
2021-07-12 16:39:58 -0600

    AccountGetId packet

--- medius/mediuspackets/accountgetid.py
+++ medius/mediuspackets/accountgetid.py
@@ -1,12 +1,27 @@
-
+from enums.enums import MediusEnum, CallbackStatus
 from utils import utils
+from medius.mediuspackets.accountgetidresponse import AccountGetIdResponseSerializer
 
 class AccountGetIdSerializer:
 	data_dict = [
-		{'name': 'mediusid', 'n_bytes': 2, 'cast': None}
+		{'name': 'mediusid', 'n_bytes': 2, 'cast': None},
+		{'name': 'message_id', 'n_bytes': MediusEnum.MESSAGEID_MAXLEN, 'cast': None},
+		{'name': 'session_key', 'n_bytes': MediusEnum.SESSIONKEY_MAXLEN, 'cast': None}
 	]
 
 class AccountGetIdHandler:
 	def process(self, serialized, monolith, con):
-		raise Exception('Unimplemented Handler: AccountGetIdHandler')
+		account_id = monolith.get_client_manager().get_account_id(session_key=serialized['session_key'])
 
+		if account_id == None:
+			return [AccountGetIdResponseSerializer.build(
+					serialized['message_id'],
+					0,
+					CallbackStatus.NO_RESULT
+				)]
+		else:
+			return [AccountGetIdResponseSerializer.build(
+					serialized['message_id'],
+					account_id,
+					CallbackStatus.SUCCESS
+				)]
--- medius/mediuspackets/accountgetidresponse.py
+++ medius/mediuspackets/accountgetidresponse.py
@@ -1,10 +1,26 @@
-
+from enums.enums import MediusIdEnum
 from utils import utils
 
 class AccountGetIdResponseSerializer:
 	data_dict = [
 		{'name': 'mediusid', 'n_bytes': 2, 'cast': None}
 	]
+
+	@classmethod
+	def build(self,
+			message_id: bytes,
+			account_id: int,
+			callback_status: int
+			):
+		packet = [
+			{'name': __name__},
+			{'mediusid': MediusIdEnum.AccountGetIdResponse},
+			{'message_id': message_id},
+			{'buf': utils.bytes_from_hex("000000")},
+			{'account_id': utils.int_to_bytes_little(4, account_id)},
+			{'callback_status': utils.int_to_bytes_little(4, callback_status)}
+		]
+		return packet
 
 class AccountGetIdResponseHandler:
 	def process(self, serialized, monolith, con):
