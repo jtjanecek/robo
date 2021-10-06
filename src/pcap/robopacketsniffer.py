91db15ec4699e470facb0a5f55854af13253cada
John Janecek <janecektyler@gmail.com>
2021-10-05 22:06:33 -0600

    Change default log sizes

--- src/pcap/robopacketsniffer.py
+++ src/pcap/robopacketsniffer.py
@@ -4,6 +4,6 @@ import shlex
 class RoboPacketSniffer:
 	def __init__(self, config: dict):
 
-		cmd = "tcpdump --interface any -s 65535 -n -x 'udp port 51000 or tcp port 10075 or tcp port 10078 or tcp port 10079 or udp port 10070' -W 10 -C 1 -w /logs/pcap.pcap"
+		cmd = "tcpdump --interface any -s 65535 -n -x 'udp port 51000 or tcp port 10075 or tcp port 10078 or tcp port 10079 or udp port 10070' -W 10 -C 30 -w /logs/pcap.pcap"
 		args = shlex.split(cmd)
 		p = subprocess.Popen(args)
