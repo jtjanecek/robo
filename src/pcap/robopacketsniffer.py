2c74c331fdafe4ffdb9e4c4d55f23017577c7a34
John Janecek <janecektyler@gmail.com>
2021-12-06 22:38:39 -0700

    Missed log count

--- src/pcap/robopacketsniffer.py
+++ src/pcap/robopacketsniffer.py
@@ -5,6 +5,6 @@ class RoboPacketSniffer:
     def __init__(self, config: dict):
 		logsize = config['pcap']['log_max_mb']
 		log_count = config['pcap']['log_backup_count']
-        cmd = f"tcpdump --interface any -s 65535 -n -x 'udp port 51000 or tcp port 10075 or tcp port 10078 or tcp port 10079 or udp port 10070' -W 10 -C {logsize} -w /logs/pcap.pcap"
+        cmd = f"tcpdump --interface any -s 65535 -n -x 'udp port 51000 or tcp port 10075 or tcp port 10078 or tcp port 10079 or udp port 10070' -W {log_count} -C {logsize} -w /logs/pcap.pcap"
         args = shlex.split(cmd)
         p = subprocess.Popen(args)
