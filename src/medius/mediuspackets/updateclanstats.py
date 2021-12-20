7963a3178d65eac3fe5b14c3ad4fec27839f1075
John Janecek <janecektyler@gmail.com>
2021-12-20 07:14:16 -0700

    Change area of checking ctags

--- src/medius/mediuspackets/updateclanstats.py
+++ src/medius/mediuspackets/updateclanstats.py
@@ -2,6 +2,7 @@ from enums.enums import MediusEnum, CallbackStatus
 from utils import utils
 from medius.mediuspackets.updateclanstatsresponse import UpdateClanStatsResponseSerializer
 
+
 class UpdateClanStatsSerializer:
     data_dict = [
         {'name': 'mediusid', 'n_bytes': 2, 'cast': None},
@@ -14,12 +15,28 @@ class UpdateClanStatsSerializer:
 
 class UpdateClanStatsHandler:
     def process(self, serialized, monolith, con):
+        clan_stats = serialized['stats']
 
-        clan_tag = serialized['stats'][172:176]
-        if not utils.check_ctag_valid(clan_tag):
-            raise Exception('Invalid clan tag!') # This will disconnect player
+        clan_tag = serialized['stats'][16:24].hex().upper()
+        ctag_valid, reason = utils.check_ctag_valid(clan_tag)
+        if not ctag_valid:
+            raise Exception(f'Invalid clan tag [{reason}]: {clan_tag}') # This will disconnect player
 
-        stats = utils.bytes_to_hex(serialized['stats'])
+        # if clan_tag not in ():
+        #     client_manager = monolith.get_client_manager()
+        #     clan_message = client_manager.get_clan_message(serialized['clan_id'])
+
+            # if clan_message == 'Colors 1':
+            #     clan_tag = bytearray(clan_tag)
+            #     # Replace all instances of colors with the mapping
+            #     for key, value in color_map_1.items():
+            #         clan_tag = clan_tag.replace(key, value)
+            #     clan_stats = bytearray(clan_stats)
+            #     clan_stats[172:176] = clan_tag
+            #     logger.info(f"Clan stats after replacement: {clan_stats}")
+
+        clan_stats = bytes(clan_stats)
+        stats = utils.bytes_to_hex(clan_stats)
         monolith.get_client_manager().update_clan_stats(serialized['clan_id'], stats)
 
         return [UpdateClanStatsResponseSerializer.build(
