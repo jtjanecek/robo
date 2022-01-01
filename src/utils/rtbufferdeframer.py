a566af503b583cf4a59d10a63aa9de3b4ae10a3b
John Janecek <janecektyler@gmail.com>
2022-01-01 09:48:26 -0700

    Use jit numba for deframer (#63)

--- src/utils/rtbufferdeframer.py
+++ src/utils/rtbufferdeframer.py
@@ -1,5 +1,40 @@
 from utils import utils
 import numpy as np
+from numba import jit
+
+
+@jit(nopython=True)
+def process(input_buf, data):
+
+    results = []
+    buf = None
+
+    if input_buf is not None:
+        data = np.concatenate((input_buf,data))
+
+    while True:
+        len_bytes = data[1:3] # Reverse it
+        len_bytes = len_bytes[0] + (len_bytes[1]*16*16) # calculate length of packet
+
+        packet_length = 1 + 2 + len_bytes
+        
+        if (data[0] & 0xFF) >= 128: # Encrypted. So we need to add the hash to the packet length
+            packet_length += 4
+
+        if packet_length == data.size: # Perfect fit
+            results.append(data)
+            return buf, results
+        elif packet_length > data.size: # This happens when the packet is cut-off
+            buf = data
+            return buf, results
+        elif packet_length < data.size: # Multiple packets here
+            results.append(data[0:packet_length])
+            data = data[packet_length:]
+        else:
+            raise Exception("Unknown error!")
+
+    return buf, results
+
 
 class RtBufferDeframer():
     def __init__(self):
@@ -7,32 +42,11 @@ class RtBufferDeframer():
 
     def deframe(self, data: bytes):
         data = np.frombuffer(data, dtype=np.uint8, count=-1)
-
-        if self._buffer is not None:
-            data = np.concatenate((self._buffer,data), axis=None)
-            self._buffer = None
-
-        results = []
-
-        while True:
-
-            packet_length = 1 + 2 + utils.bytes_to_int_little(data[1:3])
-            if (data[0] & 0xFF) >= 128: # Encrypted. So we need to add the hash to the packet length
-                packet_length += 4
-
-            if packet_length == data.size: # Perfect fit
-                results.append(data.tobytes())
-                return results
-            elif packet_length > data.size: # This happens when the packet is cut-off
-                self._buffer = data
-                return results
-            elif packet_length < data.size: # Multiple packets here
-                results.append(data[0:packet_length].tobytes())
-                data = data[packet_length:]
-            else:
-                raise Exception("Unknown error!")
-
+        buf, results = process(self._buffer, data)
+        self._buffer = buf
+        results = [result.tobytes() for result in results]
         return results
+
 
     @classmethod
     def basic_deframe(self, data: bytes):
